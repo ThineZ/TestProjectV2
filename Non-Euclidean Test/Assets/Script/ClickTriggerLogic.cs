8ed31d5b8dfecd4ce820558123cb10aa73a1cde8
ThineZ <gunpolt39@gmail.com>
2023-04-18 12:32:55 +0800

    - Added Load Page Anim
    - Added Minor Changes
    - Added Load Page Selections

--- Non-Euclidean Test/Assets/Script/ClickTriggerLogic.cs
+++ Non-Euclidean Test/Assets/Script/ClickTriggerLogic.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class ClickTriggerLogic : MonoBehaviour
@@ -11,11 +12,22 @@ public class ClickTriggerLogic : MonoBehaviour
 
     [Header("Page Items")]
     public Animator OptionsPageAnim;
+    public Animator LoadGamePageAnim;
 
 
     public void LoadGameTrigger()
     {
+        // Menu Item Slide Out Anim
+        OptionsAnim.SetBool("IfOptionTrigger", true);
+        NewGameAnim.SetBool("IfNewGameTrigger", true);
+        LoadGameAnim.SetBool("IfLoadGameTrigger", true);
+        ExitAnim.SetBool("IfExitTrigger", true);
 
+        Title.SetBool("IfTitleTrigger", true);
+
+        // Page Slide In Anim
+        LoadGamePageAnim.SetBool("IfLoadClicked", true);
+        LoadGamePageAnim.SetBool("IfLoadUnClicked", false);
     }
 
     public void OptionsTrigger()
@@ -50,6 +62,7 @@ public class ClickTriggerLogic : MonoBehaviour
             Title.SetBool("IfTitleTrigger", false);
 
             OptionsPageAnim.SetBool("IfUnClicked", true);
+            LoadGamePageAnim.SetBool("IfLoadUnClicked", true);
         }
     }
 }
