57683258f5478da5d4bcc2e81b670402f279334e
ThineZ <gunpolt39@gmail.com>
2023-04-19 20:32:36 +0800

    - Some Changes

---
+++ Non-Euclidean Test/Assets/Script/GravityChange/GravityYSwitch.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Transactions;
+using UnityEngine;
+
+public class GravityYSwitch : MonoBehaviour
+{
+    public GameObject Player;
+
+    public bool isEntered = false;
+
+    private void Update()
+    {
+        StartCoroutine(Switch());
+    }
+
+    IEnumerator Switch()
+    {
+        if (isEntered)
+        {
+            yield return new WaitForSeconds(0.01f);
+            Physics.gravity = new Vector3(0,9.81f,0);
+            //Player.transform.RotateAround(transform.position, transform.forward, -180);
+            Player.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
+        }
+
+        else if (isEntered && Player.transform.rotation == Quaternion.Euler(0,0,180))
+        {
+            yield return new WaitForSeconds(0.01f);
+            Physics.gravity = new Vector3(0,-9.81f,0);
+            Player.transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            isEntered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.name == "Player")
+        {
+            isEntered = false;
+        }
+    }
+}
